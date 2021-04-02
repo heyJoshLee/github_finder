1eb92bb45f9b4916d819852d2b32fffad436b8cc
MyMac <mymac@Macbook.local>
2021-04-02 22:47:22 +0800

    Add live url

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <>
+      <h1>About this app</h1>
+      <p>App to search githbu users </p>
+    </>
+  )
+}
+
+export default About;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Search from '../users/Search';
+import Users from '../users/Users';
+
+export const Home = () => {
+  return (
+    <>
+    <Search />
+    <Users />
+  </>
+  )
+}
+
+export default Home;
